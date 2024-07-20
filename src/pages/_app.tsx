09d725ec11a8d25f61334e6c1ecf4c1d8d1aaf04
gimminjae <min356812@naver.com>
2024-07-20 15:54:01 +0900

    apply daisyui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
+import "../app/globals.css";
 import { AppProps } from "next/app";
-import app from "@/firebase/firebase"; // Firebase SDK 구성을 위한 설정 파일 import
 import { AuthProvider } from "@/firebase/auth";
 import Layout from "@/components/Layout/Layout";
 import React from "react";
