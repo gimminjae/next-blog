877fcd0222c4b29e1e8ad12f2b6f7c23255ddcb2
gimminjae <min356812@naver.com>
2024-07-22 16:04:33 +0900

    when path includes write or edit, layout is not work

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useMemo } from "react";
 import Header from "./Header";
+import { useRouter } from "next/router";
 
 function Layout({ children }: React.PropsWithChildren<{}>) {
+  const router = useRouter()
+  useEffect(() => {
+    console.log(router)
+  }, [router.pathname])
+  const isPostWritePage = useMemo(() => router.pathname.includes('write') || router.pathname.includes('edit'), [router.pathname])
   return (
     <>
       <Header />
-      <div className="sm:container sm:mx-auto">{children}</div>
+      <div className={isPostWritePage ? "" : "sm:container sm:mx-auto"}>{children}</div>
     </>
   );
 }
