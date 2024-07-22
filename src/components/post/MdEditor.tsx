07969d73f452bb5d6b2d0c4558f167e3888fc94c
gimminjae <min356812@naver.com>
2024-07-22 14:50:46 +0900

    react-md-editor

---
+++ src/components/post/MdEditor.tsx
@@ -0,0 +1,15 @@
+import MDEditor from '@uiw/react-md-editor';
+import { useState, useCallback, ChangeEvent } from 'react'
+
+const MdEditor = ({ value, onChange}: any) => {
+    return (
+        <div className="markarea">
+	<div data-color-mode="light">
+		<MDEditor height={865} value={value} onChange={(value, viewUpdate) => {
+            onChange(value)
+}} />
+	</div>
+</div>
+    )
+}
+export default MdEditor
