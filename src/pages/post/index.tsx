5384fb599996df28429195b16eae8f8110b0a620
gimminjae <min356812@naver.com>
2024-11-09 09:36:00 +0900

    change: post page -> member page redirect

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,10 +1,17 @@
 import PostList from "@/components/post/PostList"
 import { postModel } from "@/firebase/database"
-import React from "react"
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
 import { useQuery } from "react-query"
 
 const PostPage = () => {
   const { error, data: postList } = useQuery(["posts"], postModel.getPostList)
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace("/member")
+  }, [])
+
   return (
     <div>
       <PostList error={error} postList={postList} />
