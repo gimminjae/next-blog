518f1d245e60a8f665781b6eb3bda9b35d3c2900
gimminjae <min356812@idrsystems.co.kr>
2025-02-24 12:33:45 +0900

    modify home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from "react"
 import { useRouter } from "next/router"
+import { useCustomQuery } from "@/hooks/useCustomQuery"
+import { postModel } from "@/firebase/database"
+import PostList from "@/components/post/PostList"
 
 function Home() {
+  const { error, data: postList } = useCustomQuery({
+    key: "posts",
+    queryFn: postModel.getPostList,
+  })
+
   const router = useRouter()
-  useEffect(() => {
-    router.push("/post")
-  }, [])
+
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <PostList error={error} postList={postList} />
+    </div>
   )
 }
 export default Home
